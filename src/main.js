9d1de16f2e705dd535f07d1e51cbc3f53cf65ff6
Morgunov Evgenii <58105584+MorgunovE@users.noreply.github.com>
2025-06-12 17:38:35 -0400

    fix: update Vuetify version and add Material Design Icons dependency

--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
+
 import "./assets/styles/main.scss";
 
-const app = createApp(App);
-app.use(store);
-app.use(router);
-app.use(vuetify);
-app.mount("#app");
+createApp(App).use(store).use(router).use(vuetify).mount("#app");
