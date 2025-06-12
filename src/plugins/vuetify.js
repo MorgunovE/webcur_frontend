e06f1f450850090b8dc2d08e421c7a3c78015a66
Morgunov Evgenii <58105584+MorgunovE@users.noreply.github.com>
2025-06-12 17:59:56 -0400

    fix: enhance vuetify.js by adding components and directives imports

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,13 @@
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 import { aliases, mdi } from "vuetify/iconsets/mdi";
 import "@mdi/font/css/materialdesignicons.css";
 
-export default createVuetify({
+const vuetify = createVuetify({
+  components,
+  directives,
   theme: {
     defaultTheme: "light",
     themes: {
@@ -26,3 +30,5 @@ export default createVuetify({
     sets: { mdi },
   },
 });
+
+export default vuetify;
