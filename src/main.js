4cc19327b6208787a6284e7a351afccb413635fe
Morgunov Evgenii <58105584+MorgunovE@users.noreply.github.com>
2025-06-12 16:39:56 -0400

    refactor(main): improve app initialization and include main stylesheet

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import vuetify from './plugins/vuetify';
+  import App from './App.vue';
+  import router from './router';
+  import store from './store';
+  import vuetify from './plugins/vuetify';
+  import './assets/main.scss';
 
-createApp(App)
-  .use(router)
-  .use(store)
-  .use(vuetify)
-  .mount('#app');
+  const app = createApp(App);
+  app.use(store);
+  app.use(router);
+  app.use(vuetify);
+  app.mount('#app');
