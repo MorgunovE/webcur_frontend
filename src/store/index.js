01566eec82970222bf59a4ef3ba66666c065c86f
Morgunov Evgenii <58105584+MorgunovE@users.noreply.github.com>
2025-06-12 11:25:19 -0400

    refactor store: integrate Vuex modules for better state management

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,17 @@
-import { createStore } from "vuex";
+import { createStore } from 'vuex';
 
+// Importation des modules Vuex
+import auth from './modules/auth';
+import devises from './modules/devises';
+import entreprises from './modules/entreprises';
+import favoris from './modules/favoris';
+
+// Création du store principal avec les modules
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  modules: {
+    auth,
+    devises,
+    entreprises,
+    favoris
+  }
 });
