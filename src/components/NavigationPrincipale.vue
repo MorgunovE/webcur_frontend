bd98a7886e8881d573e9a74798c4b07585ab5ab7
Morgunov Evgenii <58105584+MorgunovE@users.noreply.github.com>
2025-06-12 19:21:39 -0400

    fix: disable navigation item for companies when not authenticated

--- src/components/NavigationPrincipale.vue
+++ src/components/NavigationPrincipale.vue
@@ -4,7 +4,11 @@
       <v-list-item to="/" title="Accueil" />
       <v-list-item to="/currencies/EUR" title="Devises" />
       <v-list-item to="/stocks/AAPL" title="Actions" />
-      <v-list-item to="/companies/AAPL" title="Entreprises" />
+      <v-list-item
+        to="/companies/AAPL"
+        title="Entreprises"
+        :disabled="!estConnecte"
+      />
       <v-list-item v-if="!estConnecte" to="/login" title="Connexion" />
       <v-list-item v-if="!estConnecte" to="/register" title="Inscription" />
       <v-list-item v-if="estConnecte" to="/account" title="Mon compte" />
