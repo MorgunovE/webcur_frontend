d67dad5bbdb7f46958ce3752e2818c7125bfe7b4
Morgunov Evgenii <58105584+MorgunovE@users.noreply.github.com>
2025-06-12 11:58:59 -0400

    add navigation component: create a new Vue component for the main navigation drawer with dynamic links

---
+++ src/components/NavigationPrincipale.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-navigation-drawer app>
+    <v-list>
+      <v-list-item to="/" title="Accueil" />
+      <v-list-item to="/currencies/USD-EUR" title="Devises" />
+      <v-list-item to="/companies/AAPL" title="Entreprises" />
+      <v-list-item v-if="!estConnecte" to="/login" title="Connexion" />
+      <v-list-item v-if="!estConnecte" to="/register" title="Inscription" />
+      <v-list-item v-if="estConnecte" to="/account" title="Mon compte" />
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+
+const store = useStore();
+const estConnecte = computed(() => !!store.state.auth.token);
+</script>
