ab717ec1de158d499a835e566dd45f61de0fcf51
Morgunov Evgenii <58105584+MorgunovE@users.noreply.github.com>
2025-06-12 16:30:20 -0400

    feat(NavigationPrincipale): add link to stock details for AAPL

--- src/components/NavigationPrincipale.vue
+++ src/components/NavigationPrincipale.vue
@@ -3,6 +3,7 @@
     <v-list>
       <v-list-item to="/" title="Accueil" />
       <v-list-item to="/currencies/USD-EUR" title="Devises" />
+      <v-list-item to="/stocks/AAPL" title="Actions" />
       <v-list-item to="/companies/AAPL" title="Entreprises" />
       <v-list-item v-if="!estConnecte" to="/login" title="Connexion" />
       <v-list-item v-if="!estConnecte" to="/register" title="Inscription" />
