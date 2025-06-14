8e02df0a913332c77809208bb8c0a8f43af1c6da
Morgunov Evgenii <58105584+MorgunovE@users.noreply.github.com>
2025-06-14 09:16:01 -0400

    feat: update currency route in HeaderPrincipal.vue

--- src/components/HeaderPrincipal.vue
+++ src/components/HeaderPrincipal.vue
@@ -3,7 +3,7 @@
     <v-toolbar-title>WebCur</v-toolbar-title>
     <v-spacer />
     <v-btn to="/" text>Accueil</v-btn>
-    <v-btn to="/currencies/USD-EUR" text>Devises</v-btn>
+    <v-btn to="/currencies/EUR" text>Devises</v-btn>
     <v-btn to="/companies/AAPL" text>Entreprises</v-btn>
     <v-btn v-if="!estConnecte" to="/login" text>Connexion</v-btn>
     <v-btn v-if="!estConnecte" to="/register" text>Inscription</v-btn>
