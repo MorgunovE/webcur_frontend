750a85339eaa31d40cbada07c62d6e3e66ee6674
Morgunov Evgenii <58105584+MorgunovE@users.noreply.github.com>
2025-06-16 19:55:18 -0400

    feat: update jest configuration for improved Vue 3 support and module handling

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
+  preset: '@vue/cli-plugin-unit-jest/presets/default',
+  transform: {
+    '^.+\\.vue$': '@vue/vue3-jest',
+    '^.+\\.js$': 'babel-jest'
+  },
   transformIgnorePatterns: [
-    '/node_modules/(?!(axios)/)'
-  ]
+    '/node_modules/(?!(vuetify|axios)/)'
+  ],
+  moduleFileExtensions: ['js', 'json', 'vue'],
 };
