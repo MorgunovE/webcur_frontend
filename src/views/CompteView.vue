cd7f2c2f664419e0dc0e54ea014766de7ad71c79
Morgunov Evgenii <58105584+MorgunovE@users.noreply.github.com>
2025-06-18 18:33:36 -0400

    refactor: replace inline popular stocks section with ActionsPopulaires component in CompteView

--- src/views/CompteView.vue
+++ src/views/CompteView.vue
@@ -316,25 +316,7 @@
         </v-card>
       </v-container>
 
-      <v-container class="mt-10">
-        <h2 class="mb-4">Actions populaires</h2>
-        <v-row>
-          <v-col
-            v-for="action in actionsPopulaires.slice(0, 5)"
-            :key="action.symbole"
-            cols="12"
-            sm="6"
-            md="2"
-          >
-            <v-card class="pa-4 hoverable">
-              <v-icon size="32" color="primary">mdi-finance</v-icon>
-              <div class="font-weight-bold mt-2">{{ action.symbole }}</div>
-              <div>Prix USD: {{ action.close }}</div>
-              <div class="text-caption">Date: {{ action.date }}</div>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
+      <ActionsPopulaires :actions="actionsPopulaires" />
 
       <v-container>
        <v-card class="pa-6 mt-8" elevation="6">
@@ -472,6 +454,7 @@ import FooterPrincipal from "../components/FooterPrincipal.vue";
 import GraphiqueLignes from "../components/GraphiqueLignes.vue";
 import CompteUserCard from "../components/CompteUserCard.vue";
 import EntreprisesPopulaires from "../components/EntreprisesPopulaires.vue";
+import ActionsPopulaires from "../components/ActionsPopulaires.vue";
 
 const store = useStore();
 const deviseActive = computed(() => store.state.devises.deviseActive);
