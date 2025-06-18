50301fa54d27df4661e4118d43e5387cf9a139d2
Morgunov Evgenii <58105584+MorgunovE@users.noreply.github.com>
2025-06-18 18:19:13 -0400

    refactor: replace inline popular companies section with EntreprisesPopulaires component in CompteView

--- src/views/CompteView.vue
+++ src/views/CompteView.vue
@@ -454,29 +454,7 @@
 
       </v-container>
 
-      <v-container class="mt-10">
-        <h2 class="mb-4">Entreprises populaires</h2>
-        <v-row>
-          <v-col
-            v-for="ent in entreprisesPopulaires.slice(0, 5)"
-            :key="ent.symbole"
-            cols="12"
-            sm="6"
-            md="2"
-          >
-            <v-card class="pa-4 hoverable">
-              <v-avatar size="32" class="mb-2" v-if="ent.image">
-                <img :src="ent.image" alt="Logo" style="width: 100%; height: 100%; object-fit: cover;" />
-              </v-avatar>
-              <v-icon v-else size="32" color="primary">mdi-domain</v-icon>
-              <div class="font-weight-bold mt-2">{{ ent.companyName }}</div>
-              <div>Prix USD: {{ ent.price }}</div>
-              <div class="text-caption">Date: {{ ent.date_maj }}</div>
-              <div class="text-caption">Variation: {{ ent.change }} ({{ ent.changePercentage }})</div>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
+      <EntreprisesPopulaires :entreprises="entreprisesPopulaires" />
 
     </v-main>
     <FooterPrincipal />
@@ -493,6 +471,7 @@ import HeaderPrincipal from "../components/HeaderPrincipal.vue";
 import FooterPrincipal from "../components/FooterPrincipal.vue";
 import GraphiqueLignes from "../components/GraphiqueLignes.vue";
 import CompteUserCard from "../components/CompteUserCard.vue";
+import EntreprisesPopulaires from "../components/EntreprisesPopulaires.vue";
 
 const store = useStore();
 const deviseActive = computed(() => store.state.devises.deviseActive);
